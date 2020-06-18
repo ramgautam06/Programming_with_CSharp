69c014efb7247daafe6e4964f14a58ee8c1e9498
rgautam <rgautam@aclara.com>
2020-06-17 22:11:45 -0400

    Introduction to the C# programming

---
+++ Introduction_to_Programming/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_to_Programming
+{
+    class Program
+    {
+        public static void Goodday()
+        {
+            Console.WriteLine("Have Good Day");
+        }
+
+        public static void print_fullname()
+        {
+            string firstname = "Ram";
+            string lastname = "Gautam";
+
+            Console.WriteLine(firstname + " " + lastname);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wolrd !! I am C#");
+
+            // Modify the sample program to print "Good Day"
+            Goodday();
+
+            //Console Application that prints first and last name 
+            print_fullname();
+
+            Console.Read();
+        }
+    }
+}
